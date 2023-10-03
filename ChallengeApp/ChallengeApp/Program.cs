ef52a2c82090581ac5fee9e45f75505e184a11ff
plasekdotnet <plasek1@wp.pl>
2023-10-04 01:21:34 +0200

    dzien 4 zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int youAge = 30;
+string youName = "Ewa";
+char youGender = 'K';
+
+if (youAge < 33 && youGender == 'K')
+{
+    Console.WriteLine("Kobieta poniżej "+ youAge + " lat");
+}
+youAge = 33;
+if (youAge == 33) 
+{
+    Console.WriteLine(youName + " wiek 33 lat");
+}
+youGender = 'M';
+youAge = 15;
+if (youGender == 'M' && youAge < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
